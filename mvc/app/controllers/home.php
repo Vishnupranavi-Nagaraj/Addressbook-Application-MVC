6bcbd5fbe1da913e9e4264eb67bdbb38f27396b9
vishnupranavi <vishnupranavi.naga@aspiresys.com>
2022-08-10 10:52:25 +0530

    htaccess and homecontroller

--- mvc/app/controllers/home.php
+++ mvc/app/controllers/home.php
@@ -1,33 +1,14 @@
 <?php
-//class Home extends controller{
-  //  public function index($name=''){
-      
-    //   $user=$this->model('user');
-      // $user->name=$name;
-       //echo $user->name;
-    //}
-
-    /////////////
+require_once '../core/Controller.php';
+//  require_once '../core/database.php';
+class Home extends Controller
+{
+    function index()
+    {
+       echo "hai";
+       $this->view("home");
+       //$this->loadmodel('model');
+    }
     
-//     $obj=user;(from user)
-//     function name($obj){
-//         $data="select*from user where email="dcfer@gmail" &&pass;
-//     }
-//     if($iobj->user){
-//         true;
-// }else{
-//     false;
-
-//     ??register page message
-//     error
-    
-
-
-//     ??add button at address list
-//     design that add page at first
-    
-
-//     //model,addressmodel,
-
-//}
+}
 ?>
